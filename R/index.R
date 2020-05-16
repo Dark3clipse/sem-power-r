48a2a2ab8281becff93a876452326071ac663d37
SophiaHadash <=>
2020-05-16 19:28:23 +0200

    Fix repo
    Signed-off-by: SophiaHadash <=>

--- R/index.R
+++ R/index.R
@@ -2,80 +2,90 @@
 # includes
 suppressWarnings(suppressMessages(suppressPackageStartupMessages({
 	source("R/functions.R")
+	source("R/simulation.R");
 	library("ggplot2")
 	library("dplyr")
+	library("psych")
+	library("stats")
+	library("pracma")
+	library("whitening")
+	library("Matrix")
+	library("MASS")
+	library("sem")
+	library("lavaan")
 	require(data.table)
 })));
 
-# where is the data located?
-data_location = "/media/shadash/321cfbdb-669b-484c-bf65-5e5e3ffbb541/"
+mc = MCSimulator();
+mc = set(mc,
+	150,
+	4,
+	t(c("explanation", "feedback")),
+	t(c("gender", "age")),
+	t(c("control", "effort", "transparency", "personalization", "rec_quality", "exp_quality", "trust", "usefulness", "satisfaction")),
+	2,
+	c(0, 0, 0, 1, 0, 0, 1, 1),
+	.7, # factor loadings
+	0,  # factor correlations
+	.3 # regression path strength
+);
+#mc = generate(mc);
+#mc = score(mc);
+this=mc;
 
-# load sequences
-t <- read.table(paste(data_location, "track_features/tf_000000000000.csv", sep=""), TRUE, ",") %>% select(track_id, tempo, valence, us_popularity_estimate, acousticness, beat_strength, bounciness, danceability, energy, flatness, instrumentalness, liveness, loudness, mechanism, organism, speechiness)
-t <- rbind(t, read.table(paste(data_location, "track_features/tf_000000000001.csv", sep=""), TRUE, ",") %>% select(track_id, tempo, valence, us_popularity_estimate, acousticness, beat_strength, bounciness, danceability, energy, flatness, instrumentalness, liveness, loudness, mechanism, organism, speechiness))
-d <- read.table(paste(data_location, "training_set/log_mini.csv", sep=""), TRUE, ",")
+this = generate(this);
+m = score(this);
 
-# pre-processing
-d = d %>% inner_join(t, by=c("track_id_clean"="track_id")) %>% arrange(session_id, session_position)
-df = data.table(d)
-df[ , valence_diff := valence - shift(valence), by = session_id]
-df[ , valence_diff_abs := abs(valence - shift(valence)), by = session_id]
-df[ , tempo_diff := tempo - shift(tempo), by = session_id]
-df[ , tempo_diff_abs := abs(tempo - shift(tempo)), by = session_id]
-df[ , popularity_diff := us_popularity_estimate - shift(us_popularity_estimate), by = session_id]
-df[ , popularity_diff_abs := abs(us_popularity_estimate - shift(us_popularity_estimate)), by = session_id]
-df[ , acousticness_diff := acousticness - shift(acousticness), by = session_id]
-df[ , acousticness_diff_abs := abs(acousticness - shift(acousticness)), by = session_id]
-df[ , beat_strength_diff := beat_strength - shift(beat_strength), by = session_id]
-df[ , beat_strength_diff_abs := abs(beat_strength - shift(beat_strength)), by = session_id]
-df[ , bounciness_diff := bounciness - shift(bounciness), by = session_id]
-df[ , bounciness_diff_abs := abs(bounciness - shift(bounciness)), by = session_id]
-df[ , danceability_diff := danceability - shift(danceability), by = session_id]
-df[ , danceability_diff_abs := abs(danceability - shift(danceability)), by = session_id]
-df[ , energy_diff := energy - shift(energy), by = session_id]
-df[ , energy_diff_abs := abs(energy - shift(energy)), by = session_id]
-df[ , flatness_diff := flatness - shift(flatness), by = session_id]
-df[ , flatness_diff_abs := abs(flatness - shift(flatness)), by = session_id]
-df[ , instrumentalness_diff := instrumentalness - shift(instrumentalness), by = session_id]
-df[ , instrumentalness_diff_abs := abs(instrumentalness - shift(instrumentalness)), by = session_id]
-df[ , liveness_diff := liveness - shift(liveness), by = session_id]
-df[ , liveness_diff_abs := abs(liveness - shift(liveness)), by = session_id]
-df[ , loudness_diff := loudness - shift(loudness), by = session_id]
-df[ , loudness_diff_abs := abs(loudness - shift(loudness)), by = session_id]
-df[ , mechanism_diff := mechanism - shift(mechanism), by = session_id]
-df[ , mechanism_diff_abs := abs(mechanism - shift(mechanism)), by = session_id]
-df[ , organism_diff := organism - shift(organism), by = session_id]
-df[ , organism_diff_abs := abs(organism - shift(organism)), by = session_id]
-df[ , speechiness_diff := speechiness - shift(speechiness), by = session_id]
-df[ , speechiness_diff_abs := abs(speechiness - shift(speechiness)), by = session_id]
-df[ , valid:=session_position-shift(session_position)==1, by = session_id]
-d = setDF(df)
-rm(df)
-d = d %>% filter(valid==TRUE) %>% mutate(skipped=ifelse(not_skipped=="true", 0, 1))
+cn=c("v", "N", "factor loadings", "regression coefficients", "factor-indicator se", "factor-indicator z", "factor-indicator p", "transparency ~ explanation [est]", "std.err", "z", "p", "trust ~ transparency [est]", "std.err", "z", "p", "personalization ~ exp_quality + feedback [est1]", "std.err", "z", "p", "[est2]", "std.err", "z", "p", "rec_quality ~ personalization [est]", "std.err", "z", "p" ,"effort ~ feedback [est]", "std.err", "z", "p", "control ~ feedback [est]", "std.err", "z", "p", "usefulness ~ trust + rec_quality + effort + control [est1]", "std.err", "z", "p", "[est2]", "std.err", "z", "p", "[est3]", "std.err", "z", "p", "[est4]", "std.err", "z", "p", "satisfaction ~ usefulness [est]", "std.err", "z", "p");
+D = matrix(, ncol=55);
+colnames(D) = cn;
 
-# visualization
-#p=ggplot(d, aes(x=vdiff, color=factor(skipped, levels=c(1, 0), labels=c("true", "false")))) +
-#	geom_histogram(fill="white", alpha=0.5, position="identity")+
-#	scale_color_manual("skipped",values=c("red","blue"))
-#ggsave("dist/valence.png", p, scale=1, dpi=300)
 
-#p=ggplot(d, aes(x=tdiff, color=factor(skipped, levels=c(1, 0), labels=c("true", "false")))) +
-#	geom_histogram(fill="white", alpha=0.5, position="identity")+
-#	scale_color_manual("skipped",values=c("red","blue"))
-#ggsave("dist/tempo.png", p, scale=1, dpi=300)
+colSd <- function (x, na.rm=FALSE) apply(X=x, MARGIN=2, FUN=sd, na.rm=na.rm)
 
-#p=ggplot(d, aes(x=vdiff_abs, color=factor(skipped, levels=c(1, 0), labels=c("true", "false")))) +
-#	geom_histogram(fill="white", alpha=0.5, position="identity")+
-#	scale_color_manual("skipped",values=c("red","blue"))
-#ggsave("dist/valence_abs.png", p, scale=1, dpi=300)
+N = c(50, 50, 100, 120, 150, 180, 200, 260, 300);
+fl= c(.50, .60, .70);
+rs= c(.25, .30, .40);
+bootstraps = 25;
+for(n in 1:9){
+	this@N = N[n];
+	for (l in 1:3){
+		this@factor_loadings = fl[l];
+		for (s in 1:3){
+			this@regression_path_strength = rs[s];
 
-#p=ggplot(d, aes(x=tdiff_abs, color=factor(skipped, levels=c(1, 0), labels=c("true", "false")))) +
-#	geom_histogram(fill="white", alpha=0.5, position="identity")+
-#	scale_color_manual("skipped",values=c("red","blue"))
-#ggsave("dist/tempo_abs.png", p, scale=1, dpi=300)
+			suppressWarnings(suppressMessages(suppressPackageStartupMessages({
+			Db = matrix(, ncol=55);
+			colnames(Db) = cn;
+			for (b in 1:bootstraps){
+				this = generate(this);
+				m = score(this);
+				Di = cbind(cbind(0, this@N, this@factor_loadings, this@regression_path_strength), m$PE[2, 6:8], m$PE[37, 5:8], m$PE[38, 5:8], m$PE[39, 5:8], m$PE[40, 5:8], m$PE[41, 5:8], m$PE[42, 5:8], m$PE[43, 5:8], m$PE[44, 5:8], m$PE[45, 5:8], m$PE[46, 5:8], m$PE[47, 5:8], m$PE[48, 5:8]);
+				colnames(Di)=cn;
+				Db = rbind(Db, Di);
+			}
+			cm = t(colMeans(Db, na.rm = T));
+			sd = t(colSd(Db, na.rm=T));
 
-# statistics
-sink("dist/glm.m");
-m=glm(skipped ~ valence_diff_abs + tempo_diff_abs + popularity_diff_abs + acousticness_diff_abs + beat_strength_diff_abs + bounciness_diff_abs + danceability_diff_abs + energy_diff_abs + flatness_diff_abs + instrumentalness_diff_abs + liveness_diff_abs + loudness_diff_abs + mechanism_diff_abs + organism_diff_abs + speechiness_diff_abs, d, family = "binomial")
-summary(m)
-sink()
+			cm[1]=1
+			Di = cm;
+			colnames(Di)=cn;
+			D = rbind(D, Di);
+
+			sd[1] = 2;
+			Di = sd;
+			colnames(Di)=cn;
+			D = rbind(D, Di);
+
+			pow = 1.64 * sd;
+			pow[1] = 3;
+			Di = pow;
+			colnames(Di)=cn;
+			D = rbind(D, Di);
+})));
+		}
+	}
+}
+write.csv(D, file="data/output.csv");
+
+
